8bd53e68560ecefdd2f3b59a80b745703836d89a
0fprod <franp.dev@protonmail.com>
2025-07-15 00:18:12 +0100

    refactor: lower coverage treshold

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   coverageReporters: ['lcov', 'text'],
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
   coveragePathIgnorePatterns: [
